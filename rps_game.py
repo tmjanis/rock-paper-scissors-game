a4cadb95596f98736f0f6d57d4f63a252484ff31
tmjanis <114050662+tmjanis@users.noreply.github.com>
2022-09-21 10:33:26 +0300

    Update rps_game.py

--- rps_game.py
+++ rps_game.py
@@ -1,13 +1,23 @@
-human_turn = input ('Input human turn: ')
-computer_turn = input ('Computer turn: ')
+from multiprocessing import RLock
+import random
+turns = ['rock', 'paper', 'scissors']
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('Human wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('Human wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('Human wins!')
-else:
-    print('Computer wins!')
+while(True):
+    human_turn = input ('Enter your turn or type exit: ')
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        break
+
+    print(f'Human: {human_turn} vs {computer_turn}')
+
+    if human_turn == computer_turn:
+     print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+     print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+     print('Human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+     print('Human wins!')
+    else:
+     print('Computer wins!')
